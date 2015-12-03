f545a5160f375c2d5f05528e302b0aabbfd4a0c9
mwilli10 <williamsmorgan123@gmail.com>
2015-12-03 00:21:00 -0500

    calendar without action

--- app/src/main/java/com/example/finalapp/dibbitz/CalendarActivity.java
+++ app/src/main/java/com/example/finalapp/dibbitz/CalendarActivity.java
@@ -1,16 +1,19 @@
 package com.example.finalapp.dibbitz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Morgan on 12/2/15.
  */
 public class CalendarActivity extends Fragment {
-
+        private ImageButton mCalendar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,20 @@ public class CalendarActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_calendar, container, false);
+
+        mCalendar = (ImageButton) v.findViewById(R.id.camera_img);
+
+        mCalendar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+//                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+//                calIntent.setData(CalendarContract.Events.CONTENT_URI);
+//                startActivity(calIntent);
+                startActivity(new Intent(Intent.ACTION_VIEW, android.net.Uri.parse("content://com.android.calendar/time/")));
+
+            }
+        });
+
         return v;
 
     }
