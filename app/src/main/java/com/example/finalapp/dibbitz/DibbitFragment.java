8e72137e7c1e058e0d5d05b4d7778108d5c2a78e
mwilli10 <williamsmorgan123@gmail.com>
2015-12-09 22:37:42 -0500

    adding calendar event where user must confirm

--- app/src/main/java/com/example/finalapp/dibbitz/DibbitFragment.java
+++ app/src/main/java/com/example/finalapp/dibbitz/DibbitFragment.java
@@ -1,11 +1,15 @@
 package com.example.finalapp.dibbitz;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -26,7 +30,10 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.net.URI;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 import java.util.UUID;
 
 /**
@@ -53,6 +60,7 @@ public class DibbitFragment extends Fragment {
     private File mPhotoFile;
     private RatingBar mRatingBar;
     private EditText mDescriptionBox;
+    private Button mSaveButton;
 
 
     public static DibbitFragment newInstance(UUID dibbitId) {
@@ -179,6 +187,14 @@ public class DibbitFragment extends Fragment {
         mPhotoView = (ImageView) v.findViewById(R.id.dibbit_photo);
         updatePhotoView();
 
+        mSaveButton = (Button) v.findViewById(R.id.btn_save);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pushAppointmentsToCalender(getActivity(), mTitleField.getText().toString(), mDescriptionBox.getText().toString(), "Home", 20160404, true);
+
+            }
+        });
 
 
         return v;
@@ -242,171 +258,40 @@ public class DibbitFragment extends Fragment {
 
     }
 
+
+
+    public void pushAppointmentsToCalender(Activity curActivity, String title, String description, String location, long startDate, boolean needReminder) {
+        /***************** Event: note(without alert) *******************/
+
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        //intent.addCategory(Intent.CATEGORY_APP_CALENDAR);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra(CalendarContract.Events.TITLE, title);
+        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, location);
+        intent.putExtra(CalendarContract.Events.DESCRIPTION, description);
+
+// Setting dates
+        Calendar calDate = Calendar.getInstance();
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                calDate.getTimeInMillis());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                calDate.getTimeInMillis()+60*60*1000);
+
+// Make it a full day event
+        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false);
+
+// Making it private and shown as busy
+        intent.putExtra(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PRIVATE);
+        intent.putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+
+        startActivity(intent);
+
+
+            Toast.makeText(getContext(), "Event added Successfully",
+                    Toast.LENGTH_LONG).show();
+
+
+    }
+
+
 }
-
-
-
-//
-//package com.bignerdranch.android.criminalintent;
-//
-//import android.app.Activity;
-//import android.content.Intent;
-//import android.support.v4.app.FragmentManager;
-//import android.os.Bundle;
-//import android.support.annotation.Nullable;
-//import android.support.v4.app.Fragment;
-//import android.text.Editable;
-//import android.text.TextWatcher;
-//import android.text.format.DateFormat;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.Button;
-//import android.widget.CheckBox;
-//import android.widget.CompoundButton;
-//import android.widget.EditText;
-//
-//import java.util.Date;
-//import java.util.UUID;
-//
-//
-//public class DibbitFragment extends Fragment {
-//
-//    private static final String ARG_CRIME_ID = "crime_id";
-//    private static final String DIALOG_DATE = "DialogDate";
-//    private static final String DIALOG_TIME = "DialogTime";
-//    private static final int REQUEST_DATE = 0;
-//    private static final int REQUEST_TIME = 1;
-//
-//    private Dibbit mDibbit;
-//    private EditText mTitleField;
-//    private Button mDateButton;
-//    private CheckBox mSolvedCheckBox;
-//    private Button mTimeButton;
-//
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
-//        mDibbit = DibbitLab.get(getActivity()).getCrime(crimeId);
-//    }
-//
-//    public static DibbitFragment newInstance(UUID crimeId){
-//        Bundle args = new Bundle();
-//        args.putSerializable(ARG_CRIME_ID, crimeId);
-//
-//        DibbitFragment fragment = new DibbitFragment();
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        View v = inflater.inflate(R.layout.fragment_dibbit, container,false);
-//        mTitleField = (EditText)v.findViewById(R.id.crime_title);
-//        mTitleField.setText(mDibbit.getTitle());
-//        mTitleField.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { // Intentionally blank
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                mDibbit.setTitle(charSequence.toString());
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {  // Intentionally blank
-//            }
-//        });
-//
-//        mDateButton = (Button) v.findViewById(R.id.crime_date);
-////        mDateButton.setText(android.text.format.DateFormat.format("Month day, Year.", mDibbit.getDate()));
-//        updateDate();
-//
-//        mDateButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FragmentManager manager = getFragmentManager();
-//                DatePickerFragment dialog = DatePickerFragment.newInstance(mDibbit.getDate());
-//                dialog.setTargetFragment(DibbitFragment.this, REQUEST_DATE);
-//                dialog.show(manager, DIALOG_DATE);
-//
-//            }
-//        });
-//
-//
-//        mTimeButton = (Button) v.findViewById(R.id.crime_time);
-////        mDateButton.setText(android.text.format.DateFormat.format("Month day, Year.", mDibbit.getDate()));
-//        updateTime();
-//
-//        mTimeButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FragmentManager manager = getFragmentManager();
-//                TimePickerFragment dialog = TimePickerFragment.newInstance(mDibbit.getDate());
-//                dialog.setTargetFragment(DibbitFragment.this, REQUEST_TIME);
-//                dialog.show(manager, DIALOG_TIME);
-//            }
-//
-//        });
-//
-//
-//
-//
-//        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
-//        mSolvedCheckBox.setChecked(mDibbit.isSolved());
-//        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                //Set the crime's solved property
-//                mDibbit.setSolved(isChecked);
-//            }
-//        });
-//        return v;
-//
-//    }
-//
-//    private void editDateDialog() {
-//        FragmentManager manager = getActivity().getSupportFragmentManager();
-//        DatePickerFragment dialog = DatePickerFragment.newInstance(mDibbit.getDate());
-//        dialog.setTargetFragment(DibbitFragment.this, REQUEST_DATE);
-//        dialog.show(manager, null);
-//    }
-//
-//    private void editTimeDialog() {
-//        FragmentManager manager = getActivity().getSupportFragmentManager();
-//        TimePickerFragment dialog = TimePickerFragment.newInstance(mDibbit.getDate());
-//        dialog.setTargetFragment(DibbitFragment.this, REQUEST_TIME);
-//        dialog.show(manager, null);
-//    }
-//
-//
-//    public void onActivityResult(int requestCode, int resultCode, Intent data){
-//        if (resultCode != Activity.RESULT_OK){
-//            return;
-//        }
-//        if (requestCode == REQUEST_DATE){
-//            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
-//            mDibbit.setDate(date);
-//            updateDate();
-//        }
-//        if(requestCode == REQUEST_TIME){
-//            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
-//            mDibbit.setTime(date);
-//            updateTime();
-//        }
-//
-//
-//    }
-//
-//    private void updateDate() {
-//        mDateButton.setText(DateFormat.format("MM/dd/yyy", mDibbit.getDate()));
-//    }
-//    private void updateTime(){
-//        mTimeButton.setText(DateFormat.format("hh:mm a", mDibbit.getTime()));
-//    }
-//}
